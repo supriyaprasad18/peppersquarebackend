694810a2adb07a9ebdb5d5f96eb4918e7a56a5c2
supriya prasad <supriya.prasad@trica.co>
2023-11-10 17:55:07 +0530

    backend

---
+++ config/middlewares.js
@@ -0,0 +1,19 @@
+module.exports = [
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      header: "*",
+      origin: ["http://localhost:3000","http://localhost:1337"],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
